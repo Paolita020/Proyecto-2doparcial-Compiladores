aff5042445d4733afebef930e9c73d22e90ae030
Paolita020 <118492309+Paolita020@users.noreply.github.com>
2023-11-12 15:07:10 -0600

    Update parte03.java
    Se cambio la logica de funciones

--- parte03.java
+++ parte03.java
@@ -1,58 +1,46 @@
+    // T -> T2 T1
+    private void T() {
+        if (hayErrores)
+            return;
 
-/*
- * “parseT()”: Esta función representa la producción T en la gramática. 
- * Analiza la parte de la consulta SQL que se refiere a la tabla principal de la consulta (“parseT2()” y “parseT1()”)
- */
-private static void parseT() {
-    if (position == input.length()) {
-        return;
-    }
-
-    parseT2();
-    parseT1();
-}
-
-/*
- * “parseT2()”: Esta función representa la producción T2 en la gramática. 
- * Analiza el identificador de la tabla en la consulta SQL, en caso de haberlo (“parseT3()”).
- */
-private static void parseT2() {
-    if (position == input.length()) {
-        return;
-    }
-    if (Character.isLetter(input.charAt(position)) || Character.isDigit(input.charAt(position))) {
-        position++;
-    }
-    parseT3();
-}
-
-/*
- *  “parseT1()”: Esta función representa la producción T1 en la gramática.
- *  Analiza las columnas de las tablas en la consulta SQL que tengan una coma(,) 
- * seguido de un análisis de una tabla seleccionada(“parseT()”).
- */
-private static void parseT1() {
-    if (position == input.length()) {
-        return;
-    }
-    
-    if (input.startsWith(",", position)) {
-        position++;
-        parseT();
-    }
-}
-
-/*
- * “parseT3()”: Esta función representa la producción T3 en la gramática. 
- * Analiza el identificador de la tabla en la consulta SQL, en caso de haberlo.
- */
-private static void parseT3() {
-    if (position == input.length()) {
-        return;
-    }
-    
-    if (Character.isLetter(input.charAt(position)) || Character.isDigit(input.charAt(position))) {
-        position++;
+        T2();
+        T1();
     }
 
-}
+    // T2 -> id T3
+    private void T2() {
+        if (hayErrores)
+            return;
+
+        if (preanalisis.tipo == TipoToken.IDENTIFICADOR) {
+            match(TipoToken.IDENTIFICADOR);
+            T3();
+        } else {
+            hayErrores = true;
+            System.out.println("Se esperaba un 'identificador'");
+        }
+    }
+
+    // T1 -> ,T | Ɛ
+    private void T1() {
+        if (hayErrores)
+            return;
+
+        if (preanalisis.tipo == TipoToken.COMA) {
+            match(TipoToken.COMA);
+            T();
+        }
+    }
+
+    // T3 -> id | Ɛ
+    private void T3() {
+        if (hayErrores)
+            return;
+
+        if (preanalisis.tipo == TipoToken.IDENTIFICADOR) {
+            match(TipoToken.IDENTIFICADOR);
+        } else {
+            hayErrores = true;
+            System.out.println("Se esperaba un 'identificador'");
+        }
+    }
