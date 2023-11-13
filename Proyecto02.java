35a37ff7db8ea6e2b453c680b03e9bb5ccf469ef
Paolita020 <118492309+Paolita020@users.noreply.github.com>
2023-11-12 21:35:35 -0600

    Update Proyecto02.java
    Explicación cases y ligeros cambios

--- Proyecto02.java
+++ Proyecto02.java
@@ -49,6 +49,12 @@ Si es un no terminal, llama a la función parseProduction() para expandir el no 
 //Esta función lleva a cabo la expansión de un no terminal según las reglas de producción de la gramática. 
     private boolean parseProduction(String nonTerminal, String currentToken) {
         switch (nonTerminal) {
+
+            /* 
+            Q → select D from T. 
+            Verifica si el token actual es "select" 
+            Realiza las siguientes acciones: elimina "Q" de la pila, y luego agrega en orden los símbolos "T", "from", "D" y "select" a la pila. 
+            */
             case "Q":
                 if (currentToken.equals("select")) {
                     stack.pop();
@@ -59,7 +65,12 @@ Si es un no terminal, llama a la función parseProduction() para expandir el no 
                     return true;
                 }
                 return false;
-
+            /*
+            D → distinct P | P. 
+            Verifica si el token actual es "distinct". 
+            Realiza las siguientes acciones: elimina "D" de la pila y agrega "P" y "distinct" a la pila. 
+            Si el token actual no es "distinct", se llama a parseProduction("P", currentToken) para continuar con la expansión de la producción D → P.
+            */
             case "D":
                 if (currentToken.equals("distinct")) {
                     stack.pop();
@@ -70,6 +81,12 @@ Si es un no terminal, llama a la función parseProduction() para expandir el no 
                     return parseProduction("P", currentToken);
                 }
 
+            /*
+            P → * | A. Primero
+            Verifica si el token actual es "". 
+            Realiza las siguientes acciones: elimina "P" de la pila y agrega "" a la pila. 
+            Si el token actual no es "*", se llama a parseProduction("A", currentToken) para continuar con la expansión de la producción P → A.
+            */
             case "P":
                 if (currentToken.equals("*")) {
                     stack.pop();
@@ -79,6 +96,12 @@ Si es un no terminal, llama a la función parseProduction() para expandir el no 
                     return parseProduction("A", currentToken);
                 }
 
+            /* 
+            A → A , A1 | A1. 
+            Verifica si el token actual es "id". 
+            Realiza las siguientes acciones: elimina "A" de la pila y agrega "A1" a la pila. 
+            Si el token actual no es "id", retorna false indicando que la producción no se pudo aplicar.
+            */
             case "A":
                 if (currentToken.equals("id")) {
                     stack.pop();
@@ -87,6 +110,13 @@ Si es un no terminal, llama a la función parseProduction() para expandir el no 
                 }
                 return false;
 
+                
+            /*   
+           A1 → id A2. 
+           Verifica si el token actual es "id". 
+           Realiza las siguientes acciones: elimina "A1" de la pila y agrega "A2" y "id" en ese orden a la pila.
+           Si el token actual no es "id", retorna false indicando que la producción no se pudo aplicar.
+            */
             case "A1":
                 if (currentToken.equals("id")) {
                     stack.pop();
@@ -95,6 +125,13 @@ Si es un no terminal, llama a la función parseProduction() para expandir el no 
                     return true;
                 }
                 return false;
+
+            /*
+            A2 → . id | Ɛ. 
+            Primero, verifica si el token actual es ".". 
+            Realiza las siguientes acciones: elimina "A2" de la pila y agrega "id" y "." en ese orden a la pila. 
+            Si el token actual no es ".", simplemente elimina "A2" de la pila, lo que refleja la expansión de la producción A2 → Ɛ (épsilon).
+            */
 
             case "A2":
                 if (currentToken.equals(".")) {
@@ -107,6 +144,12 @@ Si es un no terminal, llama a la función parseProduction() para expandir el no 
                     return true;
                 }
 
+            /*   
+            T → T , T1 | T1. 
+            Verifica si el token actual es "id". 
+            Realiza las siguientes acciones: elimina "T" de la pila y agrega "T1" a la pila. 
+            Si el token actual no es "id", retorna false indicando que la producción no se pudo aplicar.
+            */
             case "T":
                 if (currentToken.equals("id")) {
                     stack.pop();
@@ -115,6 +158,12 @@ Si es un no terminal, llama a la función parseProduction() para expandir el no 
                 }
                 return false;
 
+            /*   
+            T1 → id T2. 
+            Verifica si el token actual es "id". 
+            Realiza las siguientes acciones: elimina "T1" de la pila y agrega "T2" y "id" en ese orden a la pila. 
+            Si el token actual no es "id", retorna false indicando que la producción no se pudo aplicar.
+            */
             case "T1":
                 if (currentToken.equals("id")) {
                     stack.pop();
@@ -124,6 +173,12 @@ Si es un no terminal, llama a la función parseProduction() para expandir el no 
                 }
                 return false;
 
+            /*
+            Este caso maneja la producción T2 → id | Ɛ. 
+            Verifica si el token actual es "id". 
+            Realiza las siguientes acciones: elimina "T2" de la pila y agrega "id" a la pila.
+            Si el token actual no es "id", simplemente elimina "T2" de la pila, lo que refleja la expansión de la producción
+            */
             case "T2":
                 if (currentToken.equals("id")) {
                     stack.pop();
